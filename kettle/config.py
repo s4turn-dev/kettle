60e1a7372484cfc031898e077a4aad0b62aae972
Michael Makashin <makashinmi@gmail.com>
2023-02-16 00:19:31 +0300

    implemented kettle's messages' logging

---
+++ kettle/config.py
@@ -0,0 +1,13 @@
+# Параметры чайника
+KETTLE_CAPACITY = 1.0
+TEMPERATURE_MAX = 100.0
+TEMPERATURE_MIN = 20.0
+CAPACITY = 1.0
+
+# Константы, влияющие на поведение температуры чайника
+SECONDS_TO_BOIL = 10
+TEMPERATURE_COOLING_STEP = 5.0
+
+# Пути логов
+TXT_FILEPATH = '../kettle_logs.txt'
+DB_FILEPATH = '../kettle_logs.sqlite3'
